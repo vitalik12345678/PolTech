952a9f86a1dc9bbc093efdc420da0e0f5adf6b5e
vitalik12345678 <vitalik142003@gmail.com>
2022-05-09 21:55:29 +0300

    initial

---
+++ src/main/java/com/lpnu/poly/PolyApplication.java
@@ -0,0 +1,13 @@
+package com.lpnu.poly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PolyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lpnu/poly/PolyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lpnu.poly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
