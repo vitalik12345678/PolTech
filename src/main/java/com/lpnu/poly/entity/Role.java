d05bf80af0b2aca8f455231cfc02131f4cf8cecc
vitalik12345678 <vitalik142003@gmail.com>
2022-05-10 13:05:41 +0300

    Role entity with relationship in user

---
+++ src/main/java/com/lpnu/poly/entity/Role.java
@@ -0,0 +1,25 @@
+package com.lpnu.poly.entity;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @OneToMany(mappedBy = "role")
+    private List<User> users;
+}
