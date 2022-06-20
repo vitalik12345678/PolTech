5588933e2dc45873e8b5642c942551bb97cc57a5
vitalik12345678 <vitalik142003@gmail.com>
2022-06-20 23:46:35 +0300

    Added file upload and get for file(create presigned URI for access to file)

--- src/main/java/com/lpnu/poly/service/FileService.java
+++ src/main/java/com/lpnu/poly/service/FileService.java
@@ -1,9 +1,11 @@
 package com.lpnu.poly.service;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface FileService {
 
-    ResponseEntity<?> uploadFile();
+    ResponseEntity<String> uploadFile(MultipartFile file, String title);
 
+    ResponseEntity<String> getFile(String title);
 }
